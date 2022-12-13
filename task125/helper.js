493752ae8e1c5992c72bfd03000b4d29986e55c1
Annette Bwemere <bwemereannette@gmail.com>
2022-12-14 01:07:47 +0200

    feat: bosyMassIndexTask125

--- task125/helper.js
+++ task125/helper.js
@@ -1,3 +1,12 @@
 export const getBMICategory = (bmi) => {
-  return 'Underweight';
+ if (bmi <= 18.5) {
+  return "Underweight"
+ }if (bmi < 25) {
+  return "Normal weight"
+ }if (bmi >=  25 && bmi < 30) {
+  return "Overweight"
+ }if (bmi >= 30) {
+  return "Obesity"
+ }
+
 }
