c0722dbd6fe53acfafb93a05ccaaeb3a1995149e
Annette Bwemere <bwemereannette@gmail.com>
2022-12-14 16:45:25 +0200

    feat: localizationTasks132

--- task132/functions.js
+++ task132/functions.js
@@ -1,3 +1,9 @@
 export const getLocale = (userConfig, knownLocales) => {
-  return userConfig.locale;
+
+  for (let i = 0; i < knownLocales.length; i++) {
+    if (userConfig.locale === knownLocales[i]) {
+      return userConfig.locale
+    }
+  } 
+  return "en"
 }
