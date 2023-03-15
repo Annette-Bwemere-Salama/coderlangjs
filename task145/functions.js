8305b0dd8e09efcbf996e99ae8e2d49bea5edbe7
Annette Bwemere <bwemereannette@gmail.com>
2023-03-15 20:57:18 +0200

    shortenByWords

--- task145/functions.js
+++ task145/functions.js
@@ -1,3 +1,8 @@
 export const shortenByWords = (s, n) => {
-  return s.slice(0, n);
+  if(s.length>n){
+    return s.slice(0, n)+ s + "..."  ;
+  }else if(s.length<n){
+    return s
+  }
+  return s
 }
