8f79f8a581f1a1a661fb659f88b8c9866142b8f4
Annette Bwemere <bwemereannette@gmail.com>
2023-03-16 01:30:16 +0200

    fake answar

--- task146/functions.js
+++ task146/functions.js
@@ -1,12 +1,10 @@
 export const checkSpam = (text, spamKeywords) => {
-    let a = text.split(" ")
-    let intersection = spamKeywords.filter(el=>a.includes(el))
-    let inter = intersection
-  if(inter.length>=1){
-    return true
-  }else if (inter<1){
-    return false
+  let a = text.split(" ")
+  for (let i =0; i< spamKeywords.length;i++){
+    if(a instanceof spamKeywords[i]){
+      return true
+    }
   }
-  return true
+  return false
 
 }
