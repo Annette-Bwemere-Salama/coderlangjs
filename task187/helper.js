739ae73c033d39e4b77ee48a4cfa9946e5d65eff
Annette Bwemere <bwemereannette@gmail.com>
2023-03-27 14:37:22 +0200

    object

--- task187/helper.js
+++ task187/helper.js
@@ -1,16 +1,18 @@
 export const isIdentical = (arr1, arr2) => {
 	const obj1Keys = Object.keys(arr1);
 	const obj2Keys = Object.keys(arr2);
+  const obj1value = Object.values(arr1);
+  const obj2value = Object.values(arr2)
 
-	if (obj1Keys.length == obj2Keys.length && obj1Keys === obj2Keys) {
+	if (obj1Keys.length === obj2Keys.length && obj1value === obj2value) {
 		return true;
 	}
 
-	for (let i = 0; i < obj1Keys.length; i++) {
-		if (!obj2Keys.includes(obj1Keys[i])) {
-			return false;
-		}
-	}
+	// for (let i = 0; i < obj1Keys.length; i++) {
+	// 	if (!obj2Keys.includes(obj1Keys[i])) {
+	// 		return false;
+	// 	}
+	// }
 
 	return false;
 };
