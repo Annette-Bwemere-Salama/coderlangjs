963c56d13c1784b58a36b6f5c1291794c8e9546e
Annette Bwemere <bwemereannette@gmail.com>
2023-03-27 16:09:21 +0200

    return ingretdiant error

--- task193/helper.js
+++ task193/helper.js
@@ -1,8 +1,17 @@
 export const bakeMyCake = (ingredients) => {
   console.log(`Baking a cake!`)
-
+// try {
+  if(!ingredients){
+    
+    throw ingredients
+  }
   return {
     type: 'cake',
     from: ingredients
   }
+// } catch (error) {
+//   if(!ingredients){
+//     return error
+//   }
+// }
 }
