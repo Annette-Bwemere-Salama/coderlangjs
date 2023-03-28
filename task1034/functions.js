d47d39df1937b7b3136dfc1f216f60cdea757952
Annette Bwemere <bwemereannette@gmail.com>
2023-03-28 10:05:01 +0200

    start serveur

--- task1034/functions.js
+++ task1034/functions.js
@@ -1,7 +1,6 @@
-import express from "express"
 export const startServer = (server, port) => {
-    server.listen(port,()=>{
-        console.log(`Server is running on port ${port}`);
+
+    server.listen(port, ()=>{
+        console.log(`${port} est cool`);
     })
-    
 }
--- task1034/index.js
+++ task1034/index.js
@@ -3,7 +3,4 @@ import { startServer } from './functions.js';
 
 const port = 8080;
 
-// startServer(server, port);
-server.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
-})
+startServer(server, port);
