1884e3322a5f0cbaa4fa30276bb8792e32ba4fc3
Annette Bwemere <bwemereannette@gmail.com>
2023-04-04 16:01:40 +0200

    Easy timeout

--- task165/functions.js
+++ task165/functions.js
@@ -1,3 +1,9 @@
 export const setEasyTimeout = (func, time) => {
-  func();
+  const hours = time.hours || 0;
+  const minutes = time.minutes || 0;
+  const seconds = time.seconds || 0;
+  
+  const totalMilliseconds = (hours * 60 * 60 + minutes * 60 + seconds) * 1000;
+  
+  setTimeout(func, totalMilliseconds);
 }
