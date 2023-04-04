5ff0960239e0f35b927507fc1aca6949866ba7b3
Annette Bwemere <bwemereannette@gmail.com>
2023-04-04 15:44:36 +0200

    status task

--- task147/functions.js
+++ task147/functions.js
@@ -1,15 +1,7 @@
 export const formatTaskList = (tasks) => {
-  for (let i = 0; i < tasks.length; i++)
-  {
-  if (tasks[i].status == 'DONE')
-  {
-    console.log(tasks[i].title)
-  tasks[i] = `\u2705 title: ${tasks[i].title}`;
-  }
-  else
-  {
-  tasks[i] = `\u274c title: ${tasks[i].title}`;
-  }
-  }
-  return tasks;
+
+  return tasks.map(task => {
+    const icon = task.status == "DONE" ? "\u2705" : "\u274c";
+    return `${icon} ${task.title}`;
+  });;
 }
