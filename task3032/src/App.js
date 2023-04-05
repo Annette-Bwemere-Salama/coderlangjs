7608eae0363e7a3e3b6ee311909b5c5ca6097a48
Annette Bwemere <bwemereannette@gmail.com>
2023-04-05 22:16:10 +0200

    stateName

--- task3032/src/App.js
+++ task3032/src/App.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
 class App extends React.Component {
-}
+    constructor(props) {
+      super(props);
+      this.state = { componentName: "App" };
+    }
+  
+    render() {
+      return (
+        <h1>This is the {this.state.componentName} component</h1>
+      );
+    }
+  }
 export default App;
