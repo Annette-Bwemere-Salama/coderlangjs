dabb9bf815b0f68b5c3316a6037ca2f05c66037c
Annette Bwemere <bwemereannette@gmail.com>
2023-04-05 20:37:56 +0200

    Training is the path to success

---
+++ task3024/src/components/Cities.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Cities() {
+  return (
+    <div>
+      <h2>Largest cities</h2>
+      <h4>Tokyo-Yokohama, Jakarta, Delhi, Manila</h4>
+    </div>
+  )
+}
---
+++ task3024/src/components/Countries.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Countries() {
+  return (
+    <div>
+      <h2>Largest countries</h2>
+      <h4>Russia, Canada, USA, China</h4>
+    </div>
+  )
+}
