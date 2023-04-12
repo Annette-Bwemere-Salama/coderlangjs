e41b6ed79efcc044caa4f42146b1a08d7e56b1b8
Annette Bwemere <bwemereannette@gmail.com>
2023-04-12 08:39:52 +0200

    width image react-native

--- task3522/App.js
+++ task3522/App.js
@@ -1,39 +1,14 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { View } from 'react-native';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
-  },
-  block: {
-    height: 50,
-  },
-  block1: {
-    width: '20%',
-    flex: 1,
-    backgroundColor: 'red',
-  },
-  block2: {
-    width: '40%',
-    flex: 2,
-    backgroundColor: 'blue',
-  },
-  block3: {
-    width: '60%',
-    flex: 3,
-    backgroundColor: 'green',
-  },
-});
+// const {width, heigth} = Dimensions.get('screen')
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={[styles.block, styles.block1]} />
-      <View style={[styles.block, styles.block2]} />
-      <View style={[styles.block, styles.block3]} />
+    <View style={{flex: 1, flexDirection:"row"}}>
+      <View style={{flex: 1,width: "20%", backgroundColor: 'red'}} />
+      <View style={{width: "40%",flex: 2,backgroundColor: 'blue',}} />
+      <View style={{  width: "60%",flex: 3,backgroundColor: 'green',}} />
     </View>
   );
 }
--- task3523/App.js
+++ task3523/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { View } from 'react-native';
-
+import { Dimensions } from 'react-native';
+const {height, width} = Dimensions.get('window');
 export default function App() {
   return (
     <View style={{ flex: 1, alignItems: 'flex-end' }}>
-      <View style={{ backgroundColor: '#5B37B6' , height: height * 0.25,}} />
-      <View style={{ backgroundColor: '#C8379D' ,width: width * 0.75,height: height * 0.25,}} />
+      <View style={{ backgroundColor: '#C8379D' ,width: width * 0.25,height: height * 0.25,}} />
       <View style={{ backgroundColor: '#E39A28' , width: width * 0.5,height: height * 0.25,}} />
-      <View style={{ backgroundColor: '#1A8AAD' , width: width * 0.25,height: height * 0.25,}} />
+      <View style={{ backgroundColor: '#1A8AAD' , width: width * 0.75,height: height * 0.25,}} />
+      <View style={{ backgroundColor: 'red' , height: height * 0.25, width: width}} />
     </View>
   );
 }
