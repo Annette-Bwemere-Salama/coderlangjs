9de17fc0b9c2e4611514d1937113ef308c82001e
Annette Bwemere <bwemereannette@gmail.com>
2023-04-12 10:29:21 +0200

    resiwe Image React-Native

--- task3525/App.js
+++ task3525/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Dimensions, Image } from 'react-native';
 
+const { width} = Dimensions.get('window')
+const imageWidth = width* 0.8
 export default function App() {
-  return <View style={styles.container}></View>;
+  return <View style={styles.container}>
+    {/* <View style={styles.vie1}> */}
+      <Image source={{ uri: "https://learn.coderslang.com/lecture-background-image.jpg"}} style={styles.imgS}/>
+    {/* </View> */}
+  </View>;
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
+    flex: 1, justifyContent: 'center', alignItems: 'center'
+
   },
+  vie1:{
+  },
+  imgS:{
+    flex: 1,
+    width: imageWidth, 
+    aspectRatio: 1,
+    resizeMode: "contain"
+
+  }
 });
