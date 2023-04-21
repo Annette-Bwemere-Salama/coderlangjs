db1ef1e74c4cb37159901b3244df6a9de756c649
Annette Bwemere <bwemereannette@gmail.com>
2023-04-21 16:22:31 +0200

    Object Json

--- backend/task1075/constants.js
+++ backend/task1075/constants.js
@@ -3,7 +3,45 @@ export const CONFIG = {
 
     currency: {},
 
-    producers: [],
+    producers: [
+        {
+            "id": 1,
+            "title": "Skeleton",
+            "baseCost": 10,
+            "growthRate": 1.11,
+            "baseProduction": 0.1,
+            "pictureURL": "https://learn.coderslang.com/skeleton.svg"
+        },
+        {
+            "id": 2,
+            "title": "Skeleton",
+            "baseCost": 10,
+            "growthRate": 1.11,
+            "baseProduction": 0.1,
+            "pictureURL": "https://learn.coderslang.com/skeleton.svg"
+        }, {
+            "id": 3,
+            "title": "Skeleton",
+            "baseCost": 10,
+            "growthRate": 1.11,
+            "baseProduction": 0.1,
+            "pictureURL": "https://learn.coderslang.com/skeleton.svg"
+        }, {
+            "id": 4,
+            "title": "Skeleton",
+            "baseCost": 10,
+            "growthRate": 1.11,
+            "baseProduction": 0.1,
+            "pictureURL": "https://learn.coderslang.com/skeleton.svg"
+        }, {
+            "id": 5,
+            "title": "Skeleton",
+            "baseCost": 10,
+            "growthRate": 1.11,
+            "baseProduction": 0.1,
+            "pictureURL": "https://learn.coderslang.com/skeleton.svg"
+        }, 
+    ],
 
     upgrades: [],
 
