afa8c299b99b0cfe05fab5dc9a4f1c4578a07a0e
Annette Bwemere <bwemereannette@gmail.com>
2023-04-24 12:07:41 +0200

    userId

--- backend/task1093/db.js
+++ backend/task1093/db.js
@@ -2,9 +2,23 @@ import { loadData } from './storage.js';
 
 export const getUser = (userId) => new Promise((resolve, reject) => {
   //check userId and reject if it's missing
-
+if (!userId) {
+  setTimeout(() => {
+    reject(new Error ("You lost id is missing"))
+  })
+} if(userId) {
+  
   setTimeout(() => {
     //use loadData and resolve with the user object if the id is present
-    return {};
+    // resolve(loadData());
+    // return loadData.friends.map(userId)
+    const users = loadData().users;
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].id === userId) {
+        // resolve the promise with the matching user
+        resolve(users[i]);
+      }
+    }
   }, 200);
+}
 });
