33bb3acb3ff7710118a53330f9a59059028c37c2
Annette Bwemere <bwemereannette@gmail.com>
2023-05-02 08:47:11 +0200

    log function

--- backend/task1108/functions.js
+++ backend/task1108/functions.js
@@ -1,18 +1,14 @@
 import { sleep } from "./helper";
-// export const writeLog = async (s,delay) => {
-//         const newLine = process.stdout.write("\n")
-//     s.split('').map(el =>  process.stdout.write(el));
-//         newLine
-//   await sleep(delay)
 
-// }
-
-export const writeLog = async (s, delay) => {
-    await Promise.all([...s].split("").map(async (c) => {
-        const newLine = process.stdout.write("\n")
-        process.stdout.write(c);
-        newLine
-        await sleep(delay);
-    }));
-    console.log();
+export const log =  (string) => {
+    // await Promise.all([...s].split("").map(async (c) => {
+    //     const newLine = process.stdout.write("\n")
+    //     process.stdout.write(c);
+    //     newLine
+    //     if (sleep) {
+    //         randomized(true)
+    //         await sleep(delay);
+    //     }
+    // }));
+    console.log(string);
 }
