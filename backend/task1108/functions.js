8db10e59d9e1b90fe3bf047234a248efec6a4ac3
Annette Bwemere <bwemereannette@gmail.com>
2023-05-02 09:38:06 +0200

    ternqire rendom

--- backend/task1108/functions.js
+++ backend/task1108/functions.js
@@ -1,9 +1,11 @@
 // import { sleep } from "./helper";
 
-export const writeLog =  (string) => {   
+import { sleep } from "./helper";
+export const writeLog = async (string, delay, randomized) => {   
     // return string.split('');
     for (let i = 0; i < string.length; i++) {
         process.stdout.write(string[i]);
+        await sleep(randomized ? Math.random() * delay : delay);
     }
     process.stdout.write('\n');
 }
