9851d97b86590e8628420cc43f119e8dad124e76
Annette Bwemere <bwemereannette@gmail.com>
2023-05-02 10:11:53 +0200

    delqy

--- backend/task1108/functions.js
+++ backend/task1108/functions.js
@@ -1,11 +1,10 @@
-// import { sleep } from "./helper";
-
 import { sleep } from "./helper";
-export const writeLog = async (string, delay, randomized) => {   
-    // return string.split('');
-    for (let i = 0; i < string.length; i++) {
-        process.stdout.write(string[i]);
-        await sleep(randomized ? Math.random() * delay : delay);
-    }
-    process.stdout.write('\n');
+export const funkylog = ({ delay, randomized }) => {
+    return async (str) => {
+        for (let i = 0; i < str.length; i++) {
+            process.stdout.write(str[i]);
+            await sleep(randomized ? Math.random() * delay : delay);
+        }
+        process.stdout.write('\n');
+    };
 }
